005444b390dff032c7bddf21db53985f2d668856
jbushra <122080613+jbushra@users.noreply.github.com>
2025-12-03 02:49:29 -0700

    Add SubtractionDataset implementation and training config (#8)
    * Add SubtractionDataset implementation and training config
    
    * Moved test_subtraction.py to a test folder, updated dataset to allow for rollouts
    
    ---------
    
    Co-authored-by: Bryan Chan <bryanchan13@gmail.com>

--- project/src/datasets/__init__.py
+++ project/src/datasets/__init__.py
@@ -8,3 +8,5 @@ sys.path.insert(0, parentdir)
 
 from src.datasets.dummy import DummyDataset
 from src.datasets.string_copy import CopyDataset
+
+from src.datasets.subtraction import SubtractionDataset
